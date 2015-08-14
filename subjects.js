658d6594fff7edbd1927a1ab6f00e9763b573dd1
Rein Bulaong <rein.bulaong@yahoo.com>
2015-08-14 23:57:50 +0800

    initial commit

---
+++ subjects.js
@@ -0,0 +1,103 @@
+subjectList = [
+	{
+		"code": "43009",
+		"name": "Intsik 10-11",
+		"section": "TWHFR",
+		"instructor": [
+			{
+				"ln": "Or", 
+				"fn": "Elsie Marie"}
+		],
+		"time": {
+			"start":8.5, 
+			"end":10
+		},
+		"day": [2,3,4,5],
+		"room": "PH 424"
+	},
+	{
+		"code": "52353",
+		"name": "EEE 190",
+		"section": "MIJKL3",
+		"instructor": [
+			{
+				"ln": "Gallano", 
+				"fn": "Russel John"
+			}
+		],
+		"time": {
+			"start":13, 
+			"end":17 
+		},
+		"day": [1],
+		"room": "EEEI VLC"
+	},
+	{
+		"code": "42002",
+		"name": "STS",
+		"section": "THW",
+		"instructor": [
+			{
+				"ln": "Vallejo", 
+				"fn": "Benjamin"
+			}
+		],
+		"time": {
+			"start":13, 
+			"end":14.5 
+		},
+		"day": [2,4],
+		"room": "CS Auditorium"
+	},
+	{
+		"code": "54048",
+		"name": "CE 22",
+		"section": "WFY",
+		"instructor": [
+			{
+				"ln": "Aquino", 
+				"fn": "H"
+			}
+		],
+		"time": {
+			"start":16, 
+			"end":17.5 
+		},
+		"day": [3,5],
+		"room": "MH 313"
+	},
+	{
+		"code": "43554",
+		"name": "Socio 10",
+		"section": "WFX",
+		"instructor": [
+			{
+				"ln": "Gatarin", 
+				"fn": "Gina"
+			}
+		],
+		"time": {
+			"start":14.5, 
+			"end":16 
+		},
+		"day": [3,5],
+		"room": "PH 318"
+	},
+	{
+		"code": "24942",
+		"name": "PE 2",
+		"section": "FIN THE",
+		"instructor": [
+			{
+				"ln": "Reyes", 
+				"fn": "Mary Ann"
+			}
+		],
+		"time": {
+			"start":11, 
+			"end":12 
+		},
+		"day": [2,4],
+		"room": "PE POOL1"
+	}
+]
